28243f63c8b1a8844823475e8c064a6f47c9579a
Baumann Patrick <pb083@hdm-stuttgart.de>
2023-07-26 11:15:43 +0200

    refactor to cli app with graph generation

---
+++ wrapper/src/subcommands/generate_graph.rs
@@ -0,0 +1,33 @@
+use graph_generator;
+use ayudame_core_rs::ayu_events::*;
+
+use super::generate_mem_address_from_id;
+
+pub(crate) fn run_generate_graph() {
+    let num_nodes = 1000;
+    let edges_per_node = 2;
+    let graph = graph_generator::GraphLayout::new_from_num_nodes(num_nodes, edges_per_node);
+    let edges = graph.build_edges();
+    initialize_with_frontend();
+    // send graph to ayudame 
+    for (predecessor, successor) in edges.into_iter().map(|(p, s)| (p as u64, s as u64)) {
+        ayu_event_addtask(predecessor, predecessor, 0, 0);
+        ayu_event_addtask(successor, successor, 0, 0);
+        ayu_event_adddependency(predecessor, successor, generate_mem_address_from_id(predecessor), generate_mem_address_from_id(successor));
+
+        ayu_event_addtasktoqueue(predecessor, predecessor);
+        ayu_event_preruntask(predecessor, predecessor);
+        ayu_event_runtask(predecessor);
+        ayu_event_postruntask(predecessor);
+
+        ayu_event_addtasktoqueue(successor, successor);
+        ayu_event_preruntask(successor, successor);
+        ayu_event_runtask(successor);
+        ayu_event_postruntask(successor);
+    }
+}
+
+fn initialize_with_frontend() {
+    ayu_event_preinit(0);
+    ayu_event_init(0);
+}
